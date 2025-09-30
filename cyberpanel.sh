3050454c684cb8af5a37c68be2feba50d1ee0794
jabirMahmoud <jabiralmahmoud@gmail.com>
2025-09-30 03:52:57 +0200

    Fix: Update cyberpanel.sh to use jabirMahmoud repository and master branch

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -1815,7 +1815,7 @@ if [[ $Server_Edition = "Enterprise" ]] ; then
   Enterprise_Flag="--ent ent --serial "
 fi
 
-sed -i 's|git clone https://github.com/usmannasir/cyberpanel|echo downloaded|g' install.py
+sed -i 's|git clone https://github.com/jabirMahmoud/cyberpanel|echo downloaded|g' install.py
 sed -i 's|mirror.cyberpanel.net|cyberpanel.sh|g' install.py
 
 
@@ -2264,13 +2264,13 @@ chown -R cyberpanel:cyberpanel /usr/local/CyberCP/lib64 || true
 
 Pre_Install_Setup_Git_URL() {
 if [[ $Server_Country != "CN" ]] ; then
-  Git_User="usmannasir"
-  Git_Content_URL="https://raw.githubusercontent.com/${Git_User}/cyberpanel"
+  Git_User="jabirMahmoud"
+  Git_Content_URL="https://raw.githubusercontent.com/${Git_User}/cyberpanel/master"
   Git_Clone_URL="https://github.com/${Git_User}/cyberpanel.git"
 else
-  Git_User="qtwrk"
-  Git_Content_URL="https://gitee.com/${Git_User}/cyberpanel/raw"
-  Git_Clone_URL="https://gitee.com/${Git_User}/cyberpanel.git"
+  Git_User="jabirMahmoud"
+  Git_Content_URL="https://raw.githubusercontent.com/${Git_User}/cyberpanel/master"
+  Git_Clone_URL="https://github.com/${Git_User}/cyberpanel.git"
 fi
 
 if [[ "$Debug" = "On" ]] ; then
